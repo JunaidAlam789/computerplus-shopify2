19d4cbd2213fc2096f67c9444b0b65db00367f0d
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2023-01-21 23:21:04 +0500

    Update {ShopifyProduct.handle}.jsx

--- src/pages/products/{ShopifyProduct.productType}/{ShopifyProduct.handle}.jsx
+++ src/pages/products/{ShopifyProduct.productType}/{ShopifyProduct.handle}.jsx
@@ -151,7 +151,7 @@ export default function Product({ data: { product, suggestions } }) {
             <h1 className={header}>{title}</h1>
             <p className={productDescription}>{description}</p>
             <h2 className={priceValue}>
-              <span className={hidden}>{price}</span>
+              <span>Get Quote</span>
             </h2>
             <fieldset className={optionsWrapper}>
               {hasVariants &&
